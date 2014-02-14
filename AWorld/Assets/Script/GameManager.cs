d4dff8398bdfbd46176dfeaf109e0e750c8f1964
Chargeorge <chargeorge@gmail.com>
2014-02-13 21:50:44 -0500

    added player team code and gamemanaager code

--- AWorld/Assets/Script/GameManager.cs
+++ AWorld/Assets/Script/GameManager.cs
@@ -3,6 +3,14 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	#region Statics
+	public  Color32 Team1Color;
+	public  Color32 Team2Color;
+	public  Color32 Team3Color;
+	public  Color32 Team4Color;
+
+	#endregion
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +20,6 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
 }
--- AWorld/Assets/Script/Player.cs
+++ AWorld/Assets/Script/Player.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public TeamInfo team;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +14,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void SetTeam(TeamInfo t){
+		team = t;
+		GetComponent<MeshRenderer> ().material.color = team.teamColor;
 	}
 }
