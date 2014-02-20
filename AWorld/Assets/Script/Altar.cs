0e59e4283568cfb2149d685417f35854f1712e67
Chargeorge <chargeorge@gmail.com>
2014-02-20 00:35:00 -0500

    Adding files, creating altar Enum

---
+++ AWorld/Assets/Script/Altar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Altar : MonoBehaviour {
+
+	public TeamInfo currentControllingTeam;
+	public AltarType alterType;
+	public TeamInfo firstControllingTeam;
+	public TeamInfo touchControl;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ AWorld/Assets/Script/Enums/AltarTyp.cs
@@ -0,0 +1,7 @@
+
+public enum AltarType
+{
+	Tikumose = 1,
+	Choytzol = 1 >>1,
+	Onixtal = 1 >> 2
+}
