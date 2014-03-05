9e8673eca3bf1baf7f25eb397400f3e78af0c9f4
Chargeorge <chargeorge@gmail.com>
2014-03-05 12:57:47 -0500

    Lock code and icon added

--- AWorld/Assets/Script/Player.cs
+++ AWorld/Assets/Script/Player.cs
@@ -147,7 +147,20 @@ public class Player : MonoBehaviour {
 							Pulsate ();
 								_currentState = PlayerState.influencing;
 							}
-							else if(currentTile.beacon == null || currentTile.beacon.GetComponent<Beacon>().percActionComplete < 100f){
+							
+							//DO Tile Control 
+							else if(currentTile.getLocalAltar()!=null && currentTile.getLocalAltar().isLocked == false && currentTile.getLocalAltar().networked == true){
+							
+								//TODO: add Animation
+								currentTile.getLocalAltar().isLocked = true;
+										
+									Hashtable ht = new Hashtable();
+									ht.Add("x",.5f);
+									ht.Add("y",.5f);
+									ht.Add("time",.50f);
+								iTween.ShakePosition(currentTile.gameObject, ht);
+							}
+							else if(currentTile.beacon == null || currentTile.beacon.GetComponent<Beacon>().percActionComplete < 100f && currentTile.getLocalAltar()== null){
 								Pulsate ();
 								_currentState = PlayerState.building;
 								float vpsBuildRate = sRef.vpsBaseBuild;
@@ -249,7 +262,6 @@ public class Player : MonoBehaviour {
 						if(currentTile.controllingTeam.teamNumber == team.teamNumber){
 							//Check for a beacon in progress and start building!
 							if(beaconInProgress != null){
-								Debug.Log("attempting to build");
 								float vpsBuildRate = sRef.vpsBaseBuild * getAltarBuildBoost ();
 								beaconInProgress.addBuildingProgress(vpsBuildRate);
 								if(x.HasValue){
@@ -258,7 +270,6 @@ public class Player : MonoBehaviour {
 								}
 								if(beaconInProgress.percActionComplete > 100f){
 								
-									Debug.Log("Finished?");
 									gm.StopSFX();
 									gm.PlaySFX(beaconBuilt, 1.0f);
 									beaconInProgress.finishAction();
@@ -295,7 +306,7 @@ public class Player : MonoBehaviour {
 					if(currentTile.controllingTeam != null){
 						if(currentTile.controllingTeam.teamNumber  == teamNumber)
 						{
-							Debug.Log("Adding Influence");
+							//Debug.Log("Adding Influence");
 							float test = currentTile.addInfluenceReturnOverflow( sRef.vpsBaseInfluence * getAltarInfluenceBoost() * Time.deltaTime);
 							if(test > 0){
 								_currentState = PlayerState.standing;
