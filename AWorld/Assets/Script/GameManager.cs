47d81fec8a02ffc2e94a78f74b6e537ad4c89496
Charles George <chargeorge@gmail.com>
2014-03-20 23:21:42 -0400

    Fixed issue with influence fight and super score bug

--- AWorld/Assets/Script/GameManager.cs
+++ AWorld/Assets/Script/GameManager.cs
@@ -142,7 +142,7 @@ public class GameManager : MonoBehaviour {
 
 			}
 			switch (gameMode){
-			case Mode.OneVOne:{
+			case Mode.TwoVTwo:{
 				_currentState = GameState.playing;
 				GameObject Player1 = (GameObject)Instantiate(prfbPlayer, new Vector3(0,0,0), Quaternion.identity);
 				GameObject Player2 = (GameObject)Instantiate(prfbPlayer, new Vector3(0,0,0), Quaternion.identity);
@@ -186,7 +186,7 @@ public class GameManager : MonoBehaviour {
 				victoryConditions.Add (new NetworkEnemyBase(1));
 				break;
 			}
-			case Mode.TwoVTwo:{
+			case Mode.OneVOne:{
 				_currentState = GameState.playing;
 				GameObject Player1 = (GameObject)Instantiate(prfbPlayer, new Vector3(0,0,0), Quaternion.identity);
 				GameObject Player2 = (GameObject)Instantiate(prfbPlayer, new Vector3(0,0,0), Quaternion.identity);
