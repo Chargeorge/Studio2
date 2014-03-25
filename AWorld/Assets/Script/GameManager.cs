42c9aa9ee5746892045297e62288e26bda38683e
Chargeorge <chargeorge@gmail.com>
2014-03-25 14:21:27 -0400

    fixed the showing in water issue

--- AWorld/Assets/Script/GameManager.cs
+++ AWorld/Assets/Script/GameManager.cs
@@ -247,7 +247,9 @@ public class GameManager : MonoBehaviour {
 			}
 			for(int x = 0; x<  tiles.GetLength(0); x++){
 				for(int y =0 ; y< tiles.GetLength(1); y++){
-					tiles[x,y].GetComponent<BaseTile>().tooCloseToBeacon();
+					if(tiles[x,y].GetComponent<BaseTile>().currentType != TileTypeEnum.water){
+						tiles[x,y].GetComponent<BaseTile>().tooCloseToBeacon();
+					}
 				}
 			}
 			setup = false;
