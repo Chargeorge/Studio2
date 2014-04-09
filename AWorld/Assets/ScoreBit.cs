2f9afa58fb1d5095e33c97eda5ee341b1fa18ab2
Chargeorge <chargeorge@gmail.com>
2014-04-09 15:58:23 -0400

    Added ScoreBit code

---
+++ AWorld/Assets/ScoreBit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreBit : MonoBehaviour {
+	List<GameObject> targets;
+	// Use this for initialization
+	void Start () {
+		targets = new List<GameObject>();
+		targets.Add (GameObject.Find ( "ScoreBitTarget"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 NewPos  =  Vector2.MoveTowards( (Vector2)transform.position, (Vector2)(targets[0].transform.position), .4f);
+		Vector3 NewPos3 = new Vector3(NewPos.x, NewPos.y, transform.position.z);
+		
+		
+		transform.position = NewPos3;
+	}
+}
