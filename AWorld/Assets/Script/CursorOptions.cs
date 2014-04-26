b0af6ba76e02318d57e4f7756046759a1f2415ad
joshr2121 <joshr2121@gmail.com>
2014-04-26 19:14:29 -0400

    Spinny cursors!

--- AWorld/Assets/Script/CursorOptions.cs
+++ AWorld/Assets/Script/CursorOptions.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class CursorOptions : MonoBehaviour {
 
-	public float speed;
+	public float moveSpeed;
+
+	public float movingRotateSpeed;
+	public float restingRotateSpeed;
+	private float rotateSpeed;
+	private float rotatingLeft;	//-1 if rotating right, 1 if rotating left
+
 	public GameObject options;
 	
 	// Use this for initialization
 	void Start () {
 		
 		OptionsManager optionsScript = options.GetComponent<OptionsManager>();
+		rotateSpeed = restingRotateSpeed * -1;
+		rotatingLeft = -1;
 		
 		//transform.position.x = startPos;
 		
@@ -20,8 +28,8 @@ public class CursorOptions : MonoBehaviour {
 		
 		Vector3 pos = transform.position;
 		
-		float x = Input.GetAxis("HorizontalPlayer1") * speed * Time.deltaTime;
-		float y = Input.GetAxis("VerticalPlayer1") * speed * Time.deltaTime;
+		float x = Input.GetAxis("HorizontalPlayer1") * moveSpeed * Time.deltaTime;
+		float y = Input.GetAxis("VerticalPlayer1") * moveSpeed * Time.deltaTime;
 		
 		if (!options.GetComponent<OptionsManager>().launching) pos.x += x;
 		//pos.y += y;
@@ -50,7 +58,21 @@ public class CursorOptions : MonoBehaviour {
 		}
 		
 		//transform.position = new Vector3(-0.16f, Mathf.Clamp(Time.time, 0.26F, -1.2F), -5.4f);
-		transform.position = pos;
+		transform.position = pos;			
+		
+		if (x > 0) {
+			rotateSpeed = -1 * movingRotateSpeed;
+			rotatingLeft = -1;
+		}
+		else if (x < 0) { 
+			rotateSpeed = movingRotateSpeed;
+			rotatingLeft = 1;
+		}
+		else {
+			rotateSpeed = restingRotateSpeed * rotatingLeft;
+		}
+		
+		transform.RotateAround (transform.position, Vector3.forward, rotateSpeed * Time.deltaTime);
 		
 	}
 }
