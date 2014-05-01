54439ebfeabf58c6c0ff733ef1dd7b632f74edd5
joshr2121 <joshr2121@gmail.com>
2014-05-01 00:56:40 -0400

    Added pyramids to score bar bottom

--- AWorld/Assets/Script/FinalScoreTarget.cs
+++ AWorld/Assets/Script/FinalScoreTarget.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class FinalScoreTarget : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void PlayScoreAnimation(){
 //		Debug.Log("Message recieved");
 		GetComponent<ParticleSystem>().Emit (10);
