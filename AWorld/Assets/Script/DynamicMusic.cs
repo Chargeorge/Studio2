82f0c6f4758d61a47dabc47db30f6fedc8c11751
Pierre Pierre <pierre.depaz@gmail.com>
2014-05-02 14:01:27 -0400

    Start Dynamic Music

---
+++ AWorld/Assets/Script/DynamicMusic.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicMusic : MonoBehaviour {
+
+	public TeamInfo team;
+
+	public AudioSource layer1Lo;
+	public AudioSource layer2LoMid;
+	public AudioSource layer3MidHi;
+	public AudioSource layer4Hi;
+
+	float scorePlayer1;
+	float scorePlayer2;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		//either play all the time and lerp the volume in and out
+		//or just play/stop it
+
+		Debug.Log("lol");
+
+		if(scorePlayer1 > 60 || scorePlayer2 > 60){
+			//play Layer1
+		} 
+	
+	}
+
+	public void audioLerp (AudioSource source, float target, float rate) {
+		if (Mathf.Abs (source.volume - target) <= 0.001f) {
+			source.volume = target;
+		}
+		else {
+			source.volume = Mathf.Lerp (source.volume, target, rate);
+		}
+	}
+}
