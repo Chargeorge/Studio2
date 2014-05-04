256d665fe19928c5a97943192c793e5dcb4a843a
joshr2121 <joshr2121@gmail.com>
2014-05-04 05:39:39 -0400

    Fixed pausing screwing up options screen

--- AWorld/Assets/Script/GameManager.cs
+++ AWorld/Assets/Script/GameManager.cs
@@ -95,6 +95,8 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	if(Input.GetKeyDown("r")) {
+		Time.timeScale = 1.0f;
+		Pause.paused = false;
 		Application.LoadLevel("PierreOptions");
 	}
 	
