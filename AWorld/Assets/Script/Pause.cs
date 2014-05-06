f62c08dc13cc948dd2d0ab6118e179a8d73f0250
siggijg <siggijg@gmail.com>
2014-05-05 23:25:19 -0400

    press P to pause

--- AWorld/Assets/Script/Pause.cs
+++ AWorld/Assets/Script/Pause.cs
@@ -14,7 +14,7 @@ public class Pause : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Pause")){
+		if (Input.GetButtonDown("Pause") || Input.GetKeyDown("p")){
 			if (paused == false){
 			//	Time.timeScale = 0;
 				paused = true;
